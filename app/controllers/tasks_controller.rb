e4ebd1f21f5b2cbe693b4e1af6bd609f656aa87e
Ajay Sharma <ajsharma@bestpeers.in>
2025-02-07 19:20:38 +0530

    Refactor and update controller logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,45 +1,46 @@
 class TasksController < ApplicationController
     
-    def index
-     @tasks = Task.all
-     @tasks = @tasks.where(status: params[:status]) if params[:status].present?
-     @tasks = @tasks.where(user_id: params[:user_id]) if params[:user_id].present?
-     @tasks = @tasks.where(project_id: params[:project_id]) if params[:project_id].present?
+  def index
+    @tasks = Task.all
+    @tasks = @tasks.where(status: params[:status]) if params[:status].present?
+    @tasks = @tasks.where(user_id: params[:user_id]) if params[:user_id].present?
+    @tasks = @tasks.where(project_id: params[:project_id]) if params[:project_id].present?
   
-     render json: @tasks
-    end
-        
-    def show
-     @task = Task.find(params[:id])
-     render json: @task
-    end
-        
-    def create
-      @task = Task.new(task_params)
-     if @task.save
+    render json: @tasks
+  end
+
+  def show
+    render json: @task
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
       render json: @task, status: :created
-     else
-      render json: @task.errors, status: :unprocessable_entity
-     end
-    end
-        
-    def update
-     @task = Task.find(params[:id])
-    if @task.update(task_params)
-     render json: @task
     else
-     render json: @task.errors, status: :unprocessable_entity
+      render json: @task.errors, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
     end
-        
-    def destroy
-      @task = Task.find(params[:id])
-      @task.destroy
-    end
-        
-    private
-        
-    def task_params
-     params.require(:task).permit(:name, :description, :user_id ,:project_id, :status)
-    end
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :user_id, :project_id, :status)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,40 @@
 class UsersController < ApplicationController
-    def index
-     @users = User.all
-     render json: @users
-    end
-        
-    def show
-     @user = User.find(params[:id])
-     @projects = @user.projects 
-     render json: @user , include: ['projects', 'tasks']
-    end
-        
-    def create
-     @user = User.new(user_params)
-     if @user.save
+  def index
+    render json: User.all
+  end
+
+  def show
+    render json: @user, include: ['projects', 'tasks']
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
       render json: @user, status: :created
-     else
+    else
       render json: @user.errors, status: :unprocessable_entity
-     end
     end
-        
-    def update
-      @user = User.find(params[:id])
+  end
+
+  def update
     if @user.update(user_params)
       render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
-    end
-        
-    def destroy
-     @user = User.find(params[:id])
-     @user.destroy
-    end
-        
-    private
-        
-    def user_params
-     params.require(:user).permit(:name, :email, :password,:role)
-    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
 end
